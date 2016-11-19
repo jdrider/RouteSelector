cb2bde3f3d6c87a5bca8c8fc7fcdb34425d9894e
Joe Rider <jdride51@gmail.com>
2016-11-19 12:04:33 -0600

    Configure Kotlin and covert MainActivity

---
+++ app/src/main/java/com/jrider/routeselector/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jrider.routeselector
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
