4b980f45e2a3d43cc8188d6d3f8e144a1ca11a37
Joe Rider <jdride51@gmail.com>
2016-12-08 20:31:30 -0600

    AddRoute UI

--- app/src/main/java/com/jrider/routeselector/MainActivity.kt
+++ app/src/main/java/com/jrider/routeselector/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.jrider.routeselector
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.jrider.routeselector.ui.routes.AddRouteActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_add_route_btn.setOnClickListener { view ->
+            val addRouteIntent = Intent(this, AddRouteActivity::class.java)
+
+            startActivity(addRouteIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/jrider/routeselector/ui/routes/AddRouteActivity.kt
@@ -0,0 +1,17 @@
+package com.jrider.routeselector.ui.routes
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jrider.routeselector.R
+
+class AddRouteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_route)
+
+        val addRouteFragment = AddRouteFragment()
+
+        supportFragmentManager.beginTransaction().replace(R.id.activity_add_route, addRouteFragment).commit()
+    }
+}
