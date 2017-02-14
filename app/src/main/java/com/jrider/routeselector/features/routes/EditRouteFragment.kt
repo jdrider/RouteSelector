ef2d142f532eb0220cce62ea578788b2f13b4a81
Joe Rider <jdride51@gmail.com>
2017-02-14 12:14:16 -0600

    Setup Place prediction widget.

--- app/src/main/java/com/jrider/routeselector/features/routes/EditRouteFragment.kt
+++ app/src/main/java/com/jrider/routeselector/features/routes/EditRouteFragment.kt
@@ -1,15 +1,21 @@
 package com.jrider.routeselector.features.routes
 
+import android.app.Activity
 import android.app.TimePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.*
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.location.places.ui.PlaceAutocomplete
 import com.jrider.routeselector.R
 import com.jrider.routeselector.RouteSelectorApplication
 import kotlinx.android.synthetic.main.fragment_edit_route.*
 import javax.inject.Inject
+
 
 class EditRouteFragment : Fragment(), RouteContract.View {
 
@@ -17,6 +23,10 @@ class EditRouteFragment : Fragment(), RouteContract.View {
     lateinit var presenter: RouteContract.Presenter
 
     companion object {
+
+        private const val START_POINT_PLACE_REQUEST = 45
+
+        private const val END_POINT_PLACE_REQUEST = 46
 
         private const val ROUTE_ID_KEY: String = "EditRouteFragment.RouteId"
 
@@ -79,6 +89,36 @@ class EditRouteFragment : Fragment(), RouteContract.View {
         return super.onOptionsItemSelected(item)
     }
 
+    /**
+     * Receive the result from a previous call to
+     * [.startActivityForResult].  This follows the
+     * related Activity API as described there in
+     * [Activity.onActivityResult].
+
+     * @param requestCode The integer request code originally supplied to
+     * *                    startActivityForResult(), allowing you to identify who this
+     * *                    result came from.
+     * *
+     * @param resultCode The integer result code returned by the child activity
+     * *                   through its setResult().
+     * *
+     * @param data An Intent, which can return result data to the caller
+     * *               (various data can be attached to Intent "extras").
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) {
+
+            if (requestCode == START_POINT_PLACE_REQUEST) {
+                val startPlace = PlaceAutocomplete.getPlace(context, data)
+            } else if (requestCode == END_POINT_PLACE_REQUEST) {
+                val endPlace = PlaceAutocomplete.getPlace(context, data)
+
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
 
@@ -103,7 +143,7 @@ class EditRouteFragment : Fragment(), RouteContract.View {
 
     override fun setNotificationTime(notificationTime: Int) {
 
-        val notificationTimeIndex = when(notificationTime){
+        val notificationTimeIndex = when (notificationTime) {
             15 -> 1
             30 -> 2
             else -> 0
@@ -121,25 +161,29 @@ class EditRouteFragment : Fragment(), RouteContract.View {
 
         text_add_route_departure_time.setOnClickListener {
             TimePickerDialog(context,
-                             timePickerListener,
-                             presenter.departureTimeHour(),
-                             presenter.departureTimeMinute(),
-                             false).show()
+                    timePickerListener,
+                    presenter.departureTimeHour(),
+                    presenter.departureTimeMinute(),
+                    false).show()
         }
 
         button_map_start_point.setOnClickListener {
-            Toast.makeText(context, "Start Point", Toast.LENGTH_SHORT).show()
+            openPlacePredictor(START_POINT_PLACE_REQUEST)
         }
 
         button_map_end_point.setOnClickListener {
-            Toast.makeText(context, "End Point", Toast.LENGTH_SHORT).show()
+            openPlacePredictor(END_POINT_PLACE_REQUEST)
         }
+
+        text_add_route_start_point.setOnClickListener { openPlacePredictor(START_POINT_PLACE_REQUEST) }
+
+        text_add_route_end_point.setOnClickListener { openPlacePredictor(END_POINT_PLACE_REQUEST) }
     }
 
     private fun setupArrayAdapter() {
         val notificationTimeAdapter = ArrayAdapter.createFromResource(context,
-                                                                      R.array.notification_time_options,
-                                                                      android.R.layout.simple_spinner_dropdown_item)
+                R.array.notification_time_options,
+                android.R.layout.simple_spinner_dropdown_item)
 
         spinner_add_route_notification_time.adapter = notificationTimeAdapter
     }
@@ -169,4 +213,18 @@ class EditRouteFragment : Fragment(), RouteContract.View {
             TimePickerDialog.OnTimeSetListener { timePicker, hourOfDay, minuteOfHour ->
                 presenter.routeDepartureTimeUpdated(hourOfDay, minuteOfHour)
             }
+
+    private fun openPlacePredictor(requestCode: Int) {
+
+        try {
+
+            val placePredictorIntent = PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY).build(activity)
+
+            startActivityForResult(placePredictorIntent, requestCode)
+        } catch (servicesRepairableException: GooglePlayServicesRepairableException) {
+
+        } catch(servicesNotAvailableException: GooglePlayServicesNotAvailableException) {
+
+        }
+    }
 }
